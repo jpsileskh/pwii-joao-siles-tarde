512f2b929856794bca4e040d2d255b3019089114
jpsileskh <jpsileskh@hotmail.com>
2025-05-06 15:57:26 -0300

    Aula de forms

---
+++ aula04/index.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+
+        <form action="mensagem.php" method="POST">
+            Digite sua mensagem:
+            <input type="text" name="msg"><br>
+            <input type="submit">
+        </form>
+
+    </body>
+</html>
